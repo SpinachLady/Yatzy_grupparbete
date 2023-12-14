9d143488b037ef4c046a044b4389e8019193e8d6
Astrid Rosen <astrid.flisan@gmail.com>
2023-12-14 12:52:25 +0100

    la till filewriter

--- src/Game.java
+++ src/Game.java
@@ -5,12 +5,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
